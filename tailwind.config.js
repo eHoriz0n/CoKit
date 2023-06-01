be1147054def44b4598f0a1e5265724e3255998f
CLog <88345999+CLOG9@users.noreply.github.com>
2023-06-01 20:41:04 +0200

    folder Structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],,
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myYellow: "#FFB800",
+        myMateBlack: "#1E1E1E",
+        myLightDark: "#383838",
+        myBrown: "#7D7461",
+        myWhite: "#F1FDF3",
+        myDarkYellow: "#8E6B0F",
+        myFontWhite: "#FDFFFC",
+      },
+      fontFamily: {
+        Poppins: ["Poppins"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
